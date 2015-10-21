010a4d6206c7085fdaa0ca5a3b6877dbfbaddab2
root <root@szymonpajzert.szymonpajzert>
2015-10-22 00:59:18 +0200

    Changed arytmetyka.ml, added new functions, deleted signature

--- arytmetyka.ml
+++ arytmetyka.ml
@@ -1,43 +1,23 @@
-(*module type ARYTMETYKA =
-   sig	
-      type wartosc 
-      (* Implicite zakładamy, że wszystkie argumenty typu float są liczbami *)
-      (* rzeczywistymi, tzn. są różne od infinity, neg_infinity i nan.      *)
-  
-      (* wartosc_dokladnosc x p = x +/- p% *)
-      (* war.pocz.: p > 0                  *)
-      val wartosc_dokladnosc: float -> float -> wartosc    
-
-      (* wartosc_od_do x y = [x;y]         *)
-      (* war.pocz.: x <= y                 *)
-      val wartosc_od_do: float -> float -> wartosc                            
-
-      (* wartosc_dokladna x = [x;x]        *)
-      val wartosc_dokladna: float -> wartosc   
-
-      (* in_wartosc w x = x \in w *)
-      val in_wartosc: wartosc -> float -> bool 
-
-      (* min_wartosc w = najmniejsza możliwa wartość w,   *)
-      (* lub neg_infinity jeśli brak dolnego ograniczenia.*)
-      val min_wartosc: wartosc -> float       
-
-      (* max_wartosc w = największa możliwa wartość w,    *)
-      (* lub infinity jeśli brak górnego ograniczenia.    *)
-      val max_wartosc: wartosc -> float       
-
-      (* środek przedziału od min_wartosc do max_wartosc, *)
-      (* lub nan jeśli min i max_wartosc nie są określone.*)
-      val sr_wartosc:  wartosc -> float       
-    
-      (* Operacje arytmetyczne na niedokładnych wartościach. *)
-      val plus:      wartosc -> wartosc -> wartosc  
-      val minus:     wartosc -> wartosc -> wartosc 
-      val razy:      wartosc -> wartosc -> wartosc  
-      val podzielic: wartosc -> wartosc -> wartosc
-   end;;*)
-
-module Arytmetyka (*: ARYTMETYKA*) = struct
+module Arytmetyka = struct
    type wartosc = (bool * float * float)
-   let wartosc_dokladnosc x = (true, x, x)
+   let wartosc_dokladnosc x p = (true, x-.x*.p/.100., x+.x*.p/.100.)
+   let wartosc_dokladna x = (true, x, x)
+   let wartosc_od_do x y = (true, x, y)
+   let min_wartosc x =
+      match x with
+      | (a, b, _) ->
+         if a
+         then b
+         else neg_infinity
+   let max_wartosc x =
+      match x with
+      | (a, _, b) ->
+         if a
+         then b
+         else infinity
 end;;
+
+open Arytmetyka;;
+let x = Arytmetyka.wartosc_od_do 2. 10.;;
+min_wartosc x;;
+max_wartosc x;;
