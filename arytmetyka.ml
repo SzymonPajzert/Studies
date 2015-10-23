61fe8a0e3288700d1d2c0444af16981e63057260
Szymon Pajzert <sp371329@khaki10.mimuw.edu.pl>
2015-10-23 14:31:34 +0200

    Dodany plus i typowanie

--- arytmetyka.ml
+++ arytmetyka.ml
@@ -1,10 +1,10 @@
 type wartosc = (bool * float * float)
 
-let wartosc_dokladnosc x p = (true, x-.x*.p/.100., x+.x*.p/.100.)
+let wartosc_dokladnosc x p = ((true, x-.x*.p/.100., x+.x*.p/.100.):wartosc)
 
-let wartosc_dokladna x = (true, x, x)
+let wartosc_dokladna x = ((true, x, x):wartosc)
 
-let wartosc_od_do x y = (true, x, y)
+let wartosc_od_do x y = ((true, x, y):wartosc)
 
 let min_wartosc x =
    match x with
@@ -20,10 +20,19 @@ let max_wartosc x =
       then b
       else infinity
 
-val przeciwienstwo: wartosc -> wartosc
-let przeciwienstwo x =
+let przeciwienstwo (x:wartosc) =
    match x with
    | (a, b, c) ->
       (a, - b, - c)
+
+let plus x y = 
+   let ((xa, xb, xc), (ya, yb, yc)) = (x, y)
+   in
+      if xa && xb
+      then
+         (true, xb +. yb, xc +. yc)
+         (*Jesli oba zbiory sa jednoprzedzialowe, nowe konce sa dodanymi koncami *)
+      else
+            
 
 let minus x y = plus x (przeciwienstwo y)
