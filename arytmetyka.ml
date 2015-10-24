3255931539fc9d9ece9f20cbdc1ff0d18001726b
szymonpajzert <szymonpajzert@szymonpajzert.szymonpajzert>
2015-10-24 15:15:22 +0200

    Dodano typy wariantowe

--- arytmetyka.ml
+++ arytmetyka.ml
@@ -1,38 +1,41 @@
-type wartosc = (bool * float * float)
+type wartosc =
+   Pusty |
+   Przedzial of (bool * float * float)
 (* czy zbior jest jednoprzedzialowy * mniejszy koniec * wiekszy koniec *)
 
-let wartosc_dokladnosc x p = ((true, x-.x*.p/.100., x+.x*.p/.100.):wartosc)
+let wartosc_dokladnosc x p = Prziedzial of (true, x-.x*.p/.100., x+.x*.p/.100)
 (* zbior jest jednoprzedzialowy, konce sa wyznaczone jako pesymistyczna dokladnosc *)
 
-let wartosc_dokladna x = ((true, x, x):wartosc)
+let wartosc_dokladna x = Przedzial of (true, x, x)
 (* zbior jest jednoprzedzialowy, konce sa podana liczba *)
 
-let wartosc_od_do x y = ((true, x, y):wartosc)
+let wartosc_od_do x y = Przedzial of (true, x, y)
 (* zbior jest jednoprzedzialowy, konce sa podane*)
 
-let min_wartosc x =
-   let (a, b, _) = x
-   in
-      if a
-      then b
+let min_wartosc = function
+   Pusty -> nan
+   Przedzial (xa, xb, xc) ->
+      if xa
+      then xb
       else neg_infinity
+
 (*Jesli zbior jest jednoprzedzialowy to minimum jest jego mniejszy koniec, w przeciwnym wypadku neg_infinity *)
 
-let max_wartosc x =
-   let (a, _, b) = x
-   in
-      if a
-      then b
+let max_wartosc = function
+   Pusty -> nan
+   Przedzial (xa, xb, xc)
+      if xa
+      then xc
       else infinity
 (*Jesli zbior jest jednoprzedzialowy to maximum jest jego wiekszy koniec, w przeciwnym wypadku infinity *)
 
 let przeciwienstwo (x:wartosc) =
    let (a, b, c) = x
-   in 
+   in
       ((a, -. b, -. c):wartosc)
 (* Przedzialowa liczebnosc sie nie zmienia, zmieniamy jedynie znaki koncowek *)
 
-let plus (x:wartosc) (y:wartosc) = 
+let plus (x:wartosc) (y:wartosc) =
    let ((xa, xb, xc), (ya, yb, yc)) = (x, y)
    in
       if (xa && ya)
