d3723bbcc4971a1f0681ff37cc356f9242c02cd4
SzymonPajzert <szymonpajzert@szymonpajzert.szymonpajzert>
2015-10-31 16:30:14 +0100

    Aktualizacja in_wartosc, odwracanie i dzielenie

--- arytmetyka.ml
+++ arytmetyka.ml
@@ -8,21 +8,22 @@ Jeśli fst>b, to jest to dwuprzedziałowy zbiór,
 Jeśli fst=snd=nan, to jest to zbiór pusty*)
 
 let wartosc_dokladnosc (x:float) (p:float) =
-   ((x-.x*.p/.100., x+.x*.p/.100.):wartosc)
+   let dokladnosc = abs_float (x *. p /. 100.)
+   in
+      ((x -. dokladnosc, x +. dokladnosc):wartosc)
 (* zbior jest jednoprzedzialowy, konce sa wyznaczone jako pesymistyczna dokladnosc *)
 
 let wartosc_dokladna (x:float) =
    ((x,x):wartosc)
 (* zbior jest jednoprzedzialowy, konce sa podana liczba *)
-
 let wartosc_od_do (x:float) (y:float) =
    ((x, y):wartosc)
 (* zbior jest jednoprzedzialowy, konce sa podane*)
 
-let in_wartosc (w:wartosc) (x:float) =
-   let czynalezy = (fst w <= x) && (x<=snd w)
+let in_wartosc ((fst, snd):wartosc) (x:float) =
+   let czynalezy = (fst <= x) && (x<=snd )
    in
-      if fst w > snd w
+      if fst > snd
       then not czynalezy
       else czynalezy
 
@@ -70,4 +71,28 @@ let odwrotnosc (x:wartosc) =
    match x with
    | (a,b) when a=neg_infinity && b=infinity -> ((neg_infinity,infinity):wartosc)
    | (0., 0.) -> ((nan,nan):wartosc)
-   | (a,b) -> (((1. /. b),(1. /. a)):wartosc)
+   | (a, 0.) -> (neg_infinity, 1. /. a)
+   | (a,b) -> ((1. /. b, 1. /. a):wartosc)
+
+let razy (x:wartosc) (y:wartosc) =
+   match(x,y) with
+   | ((a1, b1), (a2, b2)) when a1 <= b1 && a2 <= b2 ->
+      ((min (min (b1 *. b2) (a1 *. a2)) (min (a1 *. b2) (a2 *. b1))), (max (max (b1 *. b2) (a1 *. a2)) (max (a1 *. b2) (a2 *. b1))))
+   (*Trywialny przypadek dla jednoprzedziałowych zbiorów.*)
+
+   | ((a1, b1), (a2, b2)) when (a1 *. b1 *. (a1 -. b1) > 0.) && a2 > b2 ->
+      ((neg_infinity, infinity):wartosc)
+   (*Pierwszy zbiór zawiera otoczenie zera, drugi zawiera wystarczająco duże i małe liczby*)
+
+   | ((a1, b1), (a2, b2)) when (a2 *. b2 *. (a2 -. b2) > 0.) && a1 > b1 ->
+      ((neg_infinity, infinity):wartosc)
+   (*Pierwszy zbiór zawiera wystarczająco duże i małe liczby, drugi zawiera otoczenie zera*)
+
+   | ((a1, b1), (a2, b2)) when a1 > b1 && a2 > b2 ->
+      odwrotnosc (razy (odwrotnosc x) (odwrotnosc y))
+   (*Jeśli oba dwuprzedziałowe zbiory są bez otoczenia zera, to możemy wziąć odwrotność iloczynu ich odwrotności*)
+   | _ -> ((nan, nan):wartosc)
+   (*Jeśli żadna poprzednia opcja nie została złapana, to znaczy, że mnożyliśmy zbiór pusty*)
+
+
+let podzielic (x:wartosc) (y:wartosc) = razy x (odwrotnosc y)
