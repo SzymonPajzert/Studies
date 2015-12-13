c673a67eba232724cb69763d18590c0808c594f7
SzymonPajzert <sp371329@students.mimuw.edu.pl>
2015-12-13 20:59:03 +0100

    more tests, still not enough

--- manualtest.ml
+++ manualtest.ml
@@ -46,6 +46,16 @@ let split x set =
   if (answer1b = answer2b) then (answer1l, answer1b, answer1r), (answer2l, answer2b, answer2r)
   else raise (Inconsistent (set, "split"))
 
+let elements set =
+  let answer1 = Iset.elements (fst set) in
+  let answer2 = Naiveset.elements (snd set) in
+  try
+    let f x (b1, e1) (b2, e2) = x && b1 = b2 && e1 = e2 in
+    let result = List.fold_left2 f true answer1 answer2 in
+    if result then answer1 else raise (Inconsistent (set, "elements"))
+  with
+  | Invalid_argument _ -> raise (Inconsistent (set, "elements: output length"))
+
 let print_set set n =
   for i = 0 to n do
     print_string (if mem i set then "1 " else "0 ");
@@ -58,19 +68,23 @@ let () = assert(is_empty set); passed ()
 let set = add (1, 3) set
 let () =
   print_set set 20;
+  elements set;
   passed ()
 
 let set = add (5, 7) set
 let () =
   print_set set 20;
+  elements set;
   passed ()
 
 let set = add (3, 10) set
 let () =
   print_set set 20;
+  elements set;
   passed ()
 
 let set = remove (3, 5) set
 let () =
   print_set set 20;
+  elements set;
   passed ()
