b2b6a8c4454bb4acde735ceee2c7738ae69b4a60
SzymonPajzert <sp371329@students.mimuw.edu.pl>
2015-12-16 13:16:19 +0100

    end

--- iSet.ml
+++ iSet.ml
@@ -5,9 +5,11 @@ Testy w paczce i manualtest.ml*)
 (*Funkcje dla przedziałów*)
 type interval = int * int
 
+(*Sprawdzenie, czy nie jest to zdegenerowany przedział*)
 let is_interval (x, y) =
   x <= y
 
+(*Jeśli x należy do interwału zwraca 0, w przeciwnym wypadku zwraca z której strony leży*)
 let cmp_el x (begin_interval, end_interval) =
   if begin_interval <= x && x <= end_interval then 0
   else if x < begin_interval then -1
@@ -25,7 +27,9 @@ let interval_merge i1 i2 =
   assert (cmp i1 i2 = 0);
   min (fst i1) (fst i2), max (snd i1) (snd i2)
 
+(*Dodawanie, wykorzystywane tylko do dodawania nieujemnych wartości liczebności*)
 let ( +/ ) a b =
+  assert (a >= 0 && b >= 0);
   if b < max_int - a then a + b else max_int
 
 (*Funckje zbioru*)
@@ -37,17 +41,21 @@ type t =
 
 let empty = Empty
 
+(*Sprawdzanie, czy jest pusty zbiór*)
 let is_empty set =
   set = Empty
 
+(*Zwraca przechowywaną w wierzchołkach wysokość*)
 let height = function
   | Node (_, _, _, h, _) -> h
   | Empty -> 0
 
+(*Zwraca przechowywaną w wierzchołkach liczebność zbioru*)
 let cardinality = function
   | Node (_, _, _, _, c) -> c
   | Empty -> 0
 
+(*Tworzy drzewo*)
 let make l k r =
   let newcardinality = cardinality l +/ cardinality r +/ (snd k - fst k + 1) in
   Node (l, k, r, max (height l) (height r) + 1, newcardinality)
@@ -78,25 +86,17 @@ let bal l k r =
               make (make l k rll) rlk (make rlr rk rr))
   else make l k r
 
+(*Prosta funckja skopiowana z STD*)
 let rec min_elt = function
   | Empty -> raise Not_found
   | Node (Empty, k, _, _, _) -> k
   | Node (l, _, _, _, _) -> min_elt l
 
+(*Prosta funkcja skopiowana z STD*)
 let rec remove_min_elt = function
   | Empty -> invalid_arg "ISet.remove_min_elt"
   | Node (Empty, _, r, _, _) -> r
   | Node (l, k, r, _, _) -> bal (remove_min_elt l) k r
-
-(*Tworzy drzewo AVL z dwóch drzew AVL, przy założeniu, że pierwsze ma mniejsze
-przedziały od drugiego
-let merge t1 t2 =
-  match t1, t2 with
-  | Empty, _ -> t2
-  | _, Empty -> t1
-  | _ ->
-      let k = min_elt t2 in
-      bal t1 k (remove_min_elt t2)*)
 
 (*Dodawanie przedziału do zbioru, przy założeniu, że cmp tego przedziału i każdego innego
 jest różne od zera. Jeśli jest to zgenerowany przedział, zwraca identyczność*)
@@ -126,7 +126,7 @@ let rec join l v r =
       else if rh > lh + 2 then bal (join l v rl) rv rr
       else make l v r
 
-(*W sygnaturze*)
+(*Prosta funkcja zmodyfikowana z STD*)
 let split x set =
   let rec loop x = function
     | Empty -> (Empty, false, Empty)
@@ -140,7 +140,7 @@ let split x set =
   in
     loop x set
 
-(*W sygnaturze*)
+(*Prosta pętla skopiowana z STD*)
 let mem x set =
   let rec loop = function
     | Empty -> false
@@ -150,12 +150,14 @@ let mem x set =
   in
   loop set
 
+(*Prosta pętla skopiowana z STD*)
 let iter f set =
   let rec loop = function
     | Empty -> ()
     | Node (l, k, r, _, _) -> loop l; f k; loop r in
   loop set
 
+(*Prosta pętla skopiowana z STD*)
 let fold f set acc =
   let rec loop acc = function
     | Empty -> acc
@@ -163,17 +165,19 @@ let fold f set acc =
           loop (f k (loop acc l)) r in
   loop acc set
 
+(*Prosta pętla skopiowana z STD*)
 let elements set =
   let rec loop acc = function
     | Empty -> acc
     | Node(l, k, r, _, _) -> loop (k :: loop acc r) l in
   loop [] set
 
+(*Dzielę splitem i sprawdzam liczebność zbioru mniejszego*)
 let below x set =
-  (*rem to zbiór mniejszych od x*)
   let (rem, mem, _) = split x set in
   cardinality rem +/ (if mem then 1 else 0)
 
+(*Jak nazwa wskasuje, join bez wskazanego korzenia*)
 let nonrootjoin l r =
   match (l, r) with
   | Empty, _ -> r
@@ -182,14 +186,14 @@ let nonrootjoin l r =
     let k = min_elt r in
     join l k (remove_min_elt r)
 
+(*Używam splita do otrzymania dwóch drzew, które łączę*)
 let remove i set =
-  (*Używam splita do otrzymania dwóch drzew, które łączę*)
   let (l, _, cr) = split (fst i) set in
   let (_, _, r) = split (snd i) cr in
   nonrootjoin l r
 
 (*zwracam parę: zbiór z usuniętymi przedziałami których cmp z przedziałem
-wejściowym wynosi 0 oraz sumę tych przedziałów*)
+wejściowym wynosi 0 oraz sumę tych usuniętych przedziałów i tego przedziału*)
 let intersectremove i set =
   let rec loop x = function
     | Empty -> (Empty, x)
@@ -207,6 +211,7 @@ let intersectremove i set =
   in
     loop i set
 
+(*Zapewniam niezmiennik dla add_one, dodaję*)
 let add i set =
   let (nset, u) = intersectremove i set in
   add_one u nset
