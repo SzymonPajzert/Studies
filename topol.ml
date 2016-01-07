db71d53556e2f4cc0fd599b8077f59f7780ce95c
SzymonPajzert <sp371329@students.mimuw.edu.pl>
2016-01-07 12:17:26 +0100

    debugged topol

--- topol.ml
+++ topol.ml
@@ -1,19 +1,25 @@
 exception Cykliczne
 
-let topol (connections:('a * 'a list) list) =
-  let i = ref 0 in
-  let visited = Array.make (List.length connections) false in
-  let map_creator acc (value, dependencies) =
-    let identifier = !i in
-    i := !i + 1;
-    PMap.add value (identifier, dependencies) acc
-  in
-  let map = List.fold_left map_creator PMap.empty connections in
-  let rec dfs ~explode acc value =
-    let (index, dependencies) = PMap.find value map in
-    if visited.(index) then (if explode then raise Cykliczne else acc)
+let topol connections =
+  (*List.iter dla danej wartości dodaje do grafu informacje o niedodwiedzeniu i zależności*)
+  let graph = ref PMap.empty in
+  let add key value = graph := PMap.add key value !graph in
+  List.iter (fun (v, dep) -> add v (false, dep)) connections;
+
+  (*dfs dla niedodwiedzonego wierzchołka dokleja jego zależności i jego na początek listy.
+  Jeśli był wierzochłek ma zabronioną wartość to zwróci wyjątek gdy flaga explode jest prawdziwa*)
+  let rec dfs (explode, ban) acc v =
+    let (visit, dependencies) =
+      try PMap.find v !graph with
+      | Not_found -> (add v (false, []); (false, []))
+    in
+    if v = ban && explode then raise Cykliczne else
+    if visit then acc
     else
-      (visited.(index) <- true;
-      value :: (List.fold_left (dfs ~explode:true) acc dependencies))
+      (add v (true, []);
+      let acc_dependencies = List.fold_left (dfs (true, ban)) acc dependencies in
+      v :: acc_dependencies)
   in
-    List.fold_left (fun acc x -> dfs ~explode:false acc (fst x)) [] connections
+  (*conditioner zwiększa numer cyklu oraz usuwa dla dfs nieinteresujące zależności*)
+  let conditioner acc (value, _) = dfs (false, value) acc value in
+  List.fold_left conditioner [] connections
