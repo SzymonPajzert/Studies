4e74786b52ed09d8fa4d5d4cf8c7838ea4dfedc7
SzymonPajzert <sp371329@students.mimuw.edu.pl>
2016-01-17 12:42:11 +0100

    added working Hash.Next function

--- hash.ml
+++ hash.ml
@@ -1,4 +1,4 @@
-(*Hashowanie, dehashowanie i szukanie następnych możliwych ruchów do zadania przelewanka*)
+(*Hashowanie, dehashowanie i szukanie następnych możliwych ruchów*)
 
 module type MaxArray =
   sig
@@ -15,9 +15,10 @@ module type H =
 module Make(MA: MaxArray) =
   struct
     let maxarray = MA.capacities
+    let dim = Array.length maxarray
 
     let hash x =
-      assert (Array.length maxarray = Array.length x);
+      assert (dim = Array.length x);
       let multiplier = ref 1 in
       let result = ref 0 in
       let iterator index value =
@@ -28,15 +29,39 @@ module Make(MA: MaxArray) =
       !result
 
     let dehash x =
-      let divisor = ref 1 in
       let remainder = ref x in
       let initialiser index =
-        divisor := (maxarray.(index) + 1) * !divisor;
-        let result = !remainder mod !divisor in
+        let result = !remainder mod (maxarray.(index) + 1) in
         remainder := (!remainder - result) / (maxarray.(index) + 1);
         result
       in
-      Array.init (Array.length maxarray) initialiser
+      Array.init dim initialiser
 
-    let next x = [x]
+    let next x =
+      let result = ref [] in
+      let curstate = dehash x in
+      (* Dodawanie stanów po wylaniu wybranej szklanki oraz napałenieniu jej do pełna *)
+      for index = 0 to dim - 1 do
+        let after_bounce = Array.init dim (fun i -> if i = index then 0 else curstate.(i)) |> hash in
+        let after_full =  Array.init dim (fun i -> if i = index then maxarray.(i) else curstate.(i)) |> hash in
+        result := after_full :: after_bounce :: !result
+      done;
+      (* Dodawanie stanów po przelewaniu z jednej szklanki do drugiej*)
+      for index = 0 to dim - 1 do
+        for i = 0 to dim - 1 do
+          if i = index then ()
+          else
+            let topour = maxarray.(i) - curstate.(i) in
+            let after_pour =
+              Array.init dim (fun it ->
+                if it = i then maxarray.(it)
+                else if it = index then curstate.(it) - topour
+                else curstate.(it))
+              |> hash
+            in
+            result := after_pour :: !result
+        done
+      done;
+      (*Zwracanie wyniku*)
+      !result
   end
--- test.ml
+++ test.ml
@@ -1,12 +1,24 @@
 module Hasher = Hash.Make(struct let capacities = [|3;5;4|] end)
 open Hasher
 
-let t x =
-  Array.iter (Printf.printf "%d ") x; print_newline ();
+let print_array a =
+  Array.iter (Printf.printf "%d ") a; print_newline ()
+
+let test_hash_dehash x =
+  print_array x;
   let h = hash x in
   Printf.printf "%d\n" h;
-  h |> dehash |> Array.iter (Printf.printf "%d "); print_newline ()
+  h |> dehash |> print_array;
+  print_newline ()
+;;
+
+let test_next x =
+  print_array x;
+  let x = x |> hash in
+  List.iter (fun x -> x |> dehash |> print_array) (next x);
+  print_newline ()
 
 let () =
-  t [|0;0;0|];
-  t [|1;0;0|];
+  test_hash_dehash [|1;2;3|];
+  test_hash_dehash [|1;0;0|];
+  test_next [|1;2;3|];
