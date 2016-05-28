6036dfd7660550dd51591eec99cfc56e2b009ebe
SzymonPajzert <szymonpajzert@gmail.com>
2016-05-28 19:54:42 +0200

    Parse finished - working on pipes.

--- game.sh
+++ game.sh
@@ -6,6 +6,14 @@ function validate_int {
     if [[ $1 == *[^0-9]* ]]; then echo "invalid int $1"; exit 1; fi
 }
 
+function extract_int {
+    local t=$1
+    extract_result_1=${t%,*}
+    validate_int ${extract_result_1}
+    extract_result_2=${t#*,}
+    validate_int ${extract_result_2}
+}
+
 while (( "$#" )); do
     case $1 in
     -n)
@@ -21,14 +29,12 @@ while (( "$#" )); do
         shift 2
         ;;
     -p1)
-        validate_int $2; x1=$2
-        validate_int $3; y1=$3
-        shift 3
+        extract_int $2; x1=${extract_result_1}; y1=${extract_result_2}
+        shift 2
         ;;
     -p2)
-        validate_int $2; x2=$2
-        validate_int $3; y2=$3
-        shift 3
+        extract_int $2; x2=${extract_result_1}; y2=${extract_result_2}
+        shift 2
         ;;
     -ai1)
         if [ -x $2 ]; then ai1=$2; else exit 1; fi
@@ -51,68 +57,59 @@ if (( ${n} <= ${distance} )); then echo "too small n"; exit 1; fi
 if [ -z ${k+x} ]; then k=100; fi
 if [ -z ${s+x} ]; then s=1; fi
 
-
 # If only second pair of coordinates is given, set it as first
-swap_coordinates=0
+swap_1_and_2=0
 if [[ -z ${x1+x} && -n ${x2+x} ]]; then
     x1=${x2}; unset x2
     y1=${y2}; unset y2
-    swap_coordinates=1
+    swap_1_and_2=1
 fi
 
+# We assure that it is possible to find proper x coordinates.
+# To make all cases possible, swap randomly x and y coordinates.
+# Swap either if it is not currently possible to find good x.
+swapped=`shuf -i 0-1 -n 1`
+if (( swapped == 1 )) || (( x1 <= distance )) && (( x1 + distance >= n )); then
+    if [[ -n ${x1+x} ]]; then t=${x1}; x1=${y1}; y1=${t}; fi
+    if [[ -n ${x2+x} ]]; then t=${x2}; x2=${y2}; y2=${t}; fi
+fi
 
 # If no coordinates are given, choose pair of non contradictory coordinates
 if [ -z ${x1+x} ]; then
-    # Eliminate possibilities where first coordinate hinders choice of second
-    (( safe_range = n > 2*$distance ? n : 2*n - 2*distance ))
-    x1=`shuf -i 1-${safe_range} -n 1`
-    (( x1 += n > 2*distance ? 0 : 2*distance - n ))
-    y1=`shuf -i 1-${n} -n 1`
-
-    # We assured problem won't happen. To make it more random, swap coordinates.
-    should_swap=`shuf -i 0-1 -n 1`
-    if (( should_swap == 1 )); then
-        (( x1 = x1 + y1 )); (( y1 = x1 - y1 )); (( x1 = x1 - y1 ))
+    if (( n >= 2*distance )); then
+        x1=`shuf -i 1-${n} -n 1`
+        y1=`shuf -i 1-${n} -n 1`
+    else
+        # Eliminate possibilities where first coordinate hinders choice of second
+        (( safe_range = 2*n - 2*distance ))
+        x1=`shuf -i 1-${safe_range} -n 1`
+        if (( 2*x1 > safe_range )); then
+            (( x1 += 2*distance - n ))
+        fi
+        y1=`shuf -i 1-${n} -n 1`
     fi
-    unset should_swap
 fi
-
-echo "$x1 $y1 $x2 $y2"
 
 # If only one pair of coordinates is given, look for second one
 if [ -z ${x2+x} ]; then
-    # Values on both sides that assure coordinates are good.
-    (( x2_biggest_left = x1 > distance ? x1 - distance : 0 ))
-    (( x2_smallest_right = x1 + distance < n ? n - x1 - distance : n+1))
-
-    # Length of ranges on top and bottom
-    (( y2_top_range = y1 > distance ? y1 - distance : 0 ))
-    (( y2_bottom_range = y1 + distance < n ? n - y1 - distance : 0))
-    (( y2_range = y2_top_range + y2_bottom_range ))
-
-    x2=`shuf -i 1-${n} -n 1`
-    if (( x2 > x2_biggest_left )) && (( x2 < x2_smallest_right)) ; then
-        # If x2 is problematic, find proper y2 - eliminate those in too close
-        # neighbourhood to the y1.
-        y2=`shuf -i 1-${y2_range} -n 1`
-        if (( y2 > y2_top_range )); then (( y2 += 2*distance - 1 )); fi
-    else
-        y2=`shuf -i 1-${n} -n 1`
-    fi
+    (( x2_left_range = x1 > distance ? x1 - distance : 0 ))
+    (( x2_right_range = x1 + distance <= n ? n + 1 - x1 - distance : 0))
+    (( x2_range = x2_left_range + x2_right_range ))
+    if (( x2_range == 0 )); then echo "not possible"; exit 1; fi
+    x2=`shuf -i 1-${x2_range} -n 1`
+    if (( x2 > x2_left_range )); then (( x2 +=  n - x2_right_range)); fi
+    y2=`shuf -i 1-${n} -n 1`
 fi
 
-# TODO - wrong first coordinates
-
-echo "$x1 $y1 $x2 $y2"
-
-if (( swap_coordinates == 1 )); then
+if (( swap_1_and_2 == 1 )); then
     (( x1 = x1 + x2 )); (( x2 = x1 - x2 )); (( x1 = x1 - x2 ))
     (( y1 = y1 + y2 )); (( y2 = y1 - y2 )); (( y1 = y1 - y2 ))
-    unset swap_coordinates
 fi
 
-echo "$x1 $y1 $x2 $y2"
-
+if (( swapped == 1 )); then
+    (( x1 = x1 + y1 )); (( y1 = x1 - y1 )); (( x1 = x1 - y1 ))
+    (( x2 = x2 + y2 )); (( y2 = x2 - y2 )); (( x2 = x2 - y2 ))
+fi
 
 if (( x1 - x2 < distance )) && (( x2 - x1 < distance )) && \
    (( y1 - y2 < distance )) && (( y2 - y1 < distance )); then
@@ -120,4 +117,56 @@ if (( x1 - x2 < distance )) && (( x2 - x1 < distance )) && \
     exit 1;
 fi
 
-echo "$x1 $y1 $x2 $y2"
+echo "$n $k $s $x1 $y1 $x2 $y2"
+
+# End of parsing, start of the game.
+
+gui_in=3
+gui_out=4
+player1_in=4
+player1_out=3
+player2_in=4
+player2_out=3
+
+gui_arguments=""
+if [ -z ${ai1+x} ]; then
+    gui_arguments="${gui_arguments} -human1";
+else
+    PIPE=$(mktemp -u)
+    mkfifo ${PIPE}
+    player1_in=5
+    eval "exec ${player1_in}<>${PIPE}"
+
+    PIPE=$(mktemp -u)
+    mkfifo ${PIPE}
+    player1_in=6
+    eval "exec ${player1_out}<>${PIPE}"
+
+    ${ai1} <&${player1_in} >&${player1_out}
+fi
+
+if [ -z ${ai2+x} ]; then
+    gui_arguments="${gui_arguments} -human2";
+else
+    PIPE=$(mktemp -u)
+    mkfifo ${PIPE}
+    player2_in=5
+    eval "exec ${player2_in}<>${PIPE}"
+
+    PIPE=$(mktemp -u)
+    mkfifo ${PIPE}
+    player2_in=6
+    eval "exec ${player2_out}<>${PIPE}"
+
+    ${ai2} <&${player2_in} >&${player2_out}
+fi
+
+PIPE=$(mktemp -u)
+mkfifo ${PIPE}
+eval "exec ${gui_in}<>${PIPE}"
+
+PIPE=$(mktemp -u)
+mkfifo ${PIPE}
+eval "exec ${gui_out}<>${PIPE}"
+
+./sredniowiecze_gui_with_libs.sh ${gui_arguments} <&${gui_in} >&${gui_out} &
