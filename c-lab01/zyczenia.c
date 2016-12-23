bcbffba491fd965c64ef70bfa8a894f455740c55
Szymon Pajzert <szymonpajzert@gmail.com>
2016-12-23 15:33:37 +0100

    Works

--- c-lab01/zyczenia.c
+++ c-lab01/zyczenia.c
@@ -2,59 +2,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
 
 const int n = 8;
 
-char message[] = "Hohoho, wszystkiego najlepszego kujonku od procesu nr %d\n";
+char message[] = "Hohoho, wszystkiego najlepszego kujonku\n";
 const size_t message_size = sizeof(message);
 
 int main() {
-  int proc_number = 0;
-  int pid;
-  int created_child = 0;
-  char buffer[message_size];
+    int proc_number = 0;
+    int pid;
+    int created_child = 0;
+    char buffer[message_size];
   
-  int pipe_dsc[2];
-  int child_input, parent_output;
+    int pipe_dsc[2];
+    int child_input, parent_output;
   
-  while(proc_number < n && !created_child) {
-    if(pipe(pipe_dsc) == -1) exit(1);
+    while(proc_number < n-1 && !created_child) {
+	if(pipe(pipe_dsc) == -1) exit(1);
     
-    switch (pid = fork()) {
-    case -1: exit(1);
+	switch (pid = fork()) {
+	    
+	case -1: exit(1);
 
-    case 0:
-      proc_number++;
-      //printf("Created child %d %d\n", proc_number, created_child);
-      parent_output = pipe_dsc[0];
-      close(pipe_dsc[1]);
-      break;
+	case 0:
+	    proc_number++;
+	    //printf("Created child %d %d\n", proc_number, created_child);
+	    parent_output = pipe_dsc[0];
+	    close(pipe_dsc[1]);
+	    break;
       
-    default:
-      created_child = 1;
-      child_input = pipe_dsc[1];
-      close(pipe_dsc[0]);
-      break;
+	default:
+	    created_child = 1;
+	    child_input = pipe_dsc[1];
+	    close(pipe_dsc[0]);
+	    break;
+	}
     }
-  }
 
-  int size;
-  if(proc_number < n-1) {
+    if(proc_number == n-1) {
+	// Jeśli wpiszę tutaj 0 to wszystko działa
+	// Jeśli jednak zmienię je na 1 to dodają mi się d
+	// czyli znaki \000 zamieniają się na coś drukowalnego 
+	child_input = dup(1);
+    }
+  
+    int size;
     while(read(parent_output, buffer, sizeof(buffer)) > 0 && proc_number >= 0) {
-      write(child_input, buffer, sizeof(buffer));
+	write(child_input, buffer, sizeof(buffer));
     }
     close(parent_output);
-    sprintf(buffer, message, proc_number);
-    write(child_input, buffer, sizeof(buffer));
+    write(child_input, message, sizeof(message));
     close(child_input);
-    wait(0);
-  }
+    if(proc_number < n-1) wait(0);
+  
 
-  if(proc_number == n-1) {
-    while(read(parent_output, buffer, message_size)) {
-      printf("%s", buffer);
-    }
-    close(parent_output);
-    printf("%s", message);
-  }
+  
 }
