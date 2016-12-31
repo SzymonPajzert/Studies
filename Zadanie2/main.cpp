d0e79b717e81f0e9bd216aea2ed32b72fb8380fd
Szymon Pajzert <szymonpajzert@gmail.com>
2016-12-31 13:55:40 +0100

    Added command line args reading

--- Zadanie2/main.cpp
+++ Zadanie2/main.cpp
@@ -3,12 +3,13 @@
 
 #include "brandes.h"
 
-int main() {
-    int thread_number;
-    std::string inputFileName, outputFileName;
-    std::cin >> thread_number >> inputFileName >> outputFileName;
+int main(int argc, char* args[]) {
 
-    auto main = brandes(thread_number, inputFileName, outputFileName);
+    int thread_number = std::stoi(args[1]);
+    std::string input_file_name = args[2];
+    std::string output_file_name = args[3];
+
+    auto main = brandes(thread_number, input_file_name, output_file_name);
     main.run();
     main.save();
 
