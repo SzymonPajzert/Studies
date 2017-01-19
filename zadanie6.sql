2f1cc3ec8ae886f6808d6aacee9311d758bf75c7
Szymon Pajzert <szymonpajzert@gmail.com>
2017-01-19 21:56:03 +0100

    zadanie6 not finished

---
+++ zadanie6.sql
@@ -0,0 +1,90 @@
+#standardSQL
+# https://www.quora.com/How-can-you-calculate-the-length-of-the-day-on-Earth-at-a-given-latitude-on-a-given-date-of-the-year
+CREATE TEMPORARY FUNCTION days(d DATE)
+RETURNS INT64 AS (
+		CASE
+			WHEN 
+        (MOD(EXTRACT(YEAR FROM d),4) = 0 AND MOD((EXTRACT(YEAR FROM d)),100) != 0)
+				OR MOD((EXTRACT(YEAR FROM d)),400) = 0 
+        THEN 365
+			ELSE 366
+		END
+);
+
+CREATE TEMPORARY FUNCTION rad(ang FLOAT64)
+RETURNS FLOAT64 AS (3.141592 * ang / 180);
+
+CREATE TEMPORARY FUNCTION delta(d DATE)
+RETURNS FLOAT64 AS (
+		rad(-23.45)  * cos(rad((360 / days(d)) * (EXTRACT(DAYOFYEAR FROM d) + 10)))
+);
+
+CREATE TEMPORARY FUNCTION psi()
+RETURNS FLOAT64 AS (rad(40.748433));
+
+CREATE TEMPORARY FUNCTION sun_equation(d DATE)
+RETURNS FLOAT64 AS (ACOS(- TAN(psi()) * TAN(delta(d))));
+
+CREATE TEMPORARY FUNCTION hour(f FLOAT64)
+RETURNS INT64 AS (CAST(TRUNC(f) AS INT64));
+
+CREATE TEMPORARY FUNCTION minute(f FLOAT64)
+RETURNS INT64 AS (CAST(TRUNC((f - hour(f)) * 60) AS INT64));
+
+CREATE TEMPORARY FUNCTION second(f FLOAT64)
+RETURNS INT64 AS (CAST(TRUNC(((f - hour(f)) * 60 - minute(f)) * 60) AS INT64));
+
+CREATE TEMPORARY FUNCTION float_to_time(f FLOAT64)
+RETURNS TIME AS (TIME(hour(f), minute(f), second(f)));
+
+CREATE TEMPORARY FUNCTION time_to_float(t TIME)
+RETURNS FLOAT64 AS (
+		EXTRACT(HOUR FROM t) + EXTRACT(MINUTE FROM t) / 60 + EXTRACT(SECOND FROM t) / 3600
+);
+
+CREATE TEMPORARY FUNCTION noon(d DATE)
+RETURNS FLOAT64 AS (
+		CASE
+			WHEN EXTRACT(MONTH FROM d) < 3 OR EXTRACT(MONTH FROM d) > 10 THEN 12
+			ELSE 13
+		END
+);
+
+CREATE TEMPORARY FUNCTION halfday(d DATE)
+RETURNS FLOAT64 AS (sun_equation(d) * 12 / 3.1415);
+
+CREATE TEMPORARY FUNCTION sunrise(d DATE)
+RETURNS FLOAT64 AS (noon(d) - halfday(d));
+
+CREATE TEMPORARY FUNCTION sunset(d DATE)
+RETURNS FLOAT64 AS (noon(d) + halfday(d));
+
+CREATE TEMPORARY FUNCTION closest(t FLOAT64, a FLOAT64, b FLOAT64)
+RETURNS FLOAT64 AS (
+		CASE
+			WHEN t < a THEN t - a
+			WHEN t > b THEN b - t
+			ELSE 
+        (CASE WHEN t - a < b - t THEN t - a ELSE b - t END)
+		END
+);
+
+WITH 
+  sun_dates AS (
+  SELECT DISTINCT
+    DATE(starttime) as d, 
+    sunrise(DATE(starttime)) as sunrise, 
+    sunset(DATE(starttime)) as sunset 
+  FROM `tag2016-bd.new_york.citibike_trips`),
+  
+  times AS (
+  SELECT * FROM 
+    (SELECT DATE(starttime) as d, starttime as timestamp, time_to_float(TIME(starttime, "America/New_York")) as time FROM `tag2016-bd.new_york.citibike_trips`)
+    UNION ALL
+    (SELECT DATE(starttime) as d, stoptime as timestamp, time_to_float(TIME(stoptime, "America/New_York")) as time FROM `tag2016-bd.new_york.citibike_trips`))
+  
+SELECT * FROM (
+  SELECT times.d, times.timestamp, times.time, sun_dates.sunrise, sun_dates.sunset,
+  6 * closest(times.time, sun_dates.sunrise, sun_dates.sunset)
+  FROM times JOIN sun_dates ON sun_dates.d = times.d
+)
