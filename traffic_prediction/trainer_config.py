9eb0ee239a4178a1db4025e259622f1d356e90d8
Szymon Pajzert <szymonpajzert@gmail.com>
2017-03-02 12:34:40 +0100

    simple_lstm

--- traffic_prediction/trainer_config.py
+++ traffic_prediction/trainer_config.py
@@ -21,9 +21,6 @@ process = 'process' if not is_predict else 'process_predict'
 define_py_data_sources2(
     train_list=trn, test_list=tst, module="dataprovider", obj=process)
 ################################### Parameter Configuaration #######################################
-TERM_NUM = 24
-FORECASTING_NUM = 24
-emb_size = 16
 batch_size = 128 if not is_predict else 1
 settings(
     batch_size=batch_size,
@@ -31,22 +28,18 @@ settings(
     learning_method=RMSPropOptimizer())
 ################################### Algorithm Configuration ########################################
 
-output_label = []
-
 link_encode = data_layer(name='link_encode', size=TERM_NUM)
-for i in xrange(FORECASTING_NUM):
-    # Each task share same weight.
-    link_param = ParamAttr(
-        name='_link_vec.w', initial_max=1.0, initial_min=-1.0)
-    link_vec = fc_layer(input=link_encode, size=emb_size, param_attr=link_param)
-    score = fc_layer(input=link_vec, size=4, act=SoftmaxActivation())
-    if is_predict:
-        maxid = maxid_layer(score)
-        output_label.append(maxid)
-    else:
-        # Multi-task training.
-        label = data_layer(name='label_%dmin' % ((i + 1) * 5), size=4)
-        cls = classification_cost(
-            input=score, name="cost_%dmin" % ((i + 1) * 5), label=label)
-        output_label.append(cls)
+
+score = simple_lstm(input=link_encode, size=4, act=SoftmaxActivation())
+
+if is_predict:
+    maxid = maxid_layer(score)
+    output_label = [maxid]
+else:
+    # Multi-task training.
+    label = data_layer(name='label_%dmin' % ((i + 1) * 5), size=4)
+    cls = classification_cost(
+        input=score, name="cost_%dmin" % ((i + 1) * 5), label=label)
+    output_label = [cls]
+
 outputs(output_label)
