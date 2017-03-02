3d1440338ebcd51e17fe4cb1dabcf16fc782d51d
Szymon Pajzert <szymonpajzert@gmail.com>
2017-03-02 09:11:27 +0100

    new dataprovider

---
+++ traffic_prediction/consts.py
@@ -0,0 +1,8 @@
+# Number of previous dates included in model
+TERM_NUM = 10
+
+# Number of skips we have to perform to get to date in a next day
+DATES_IN_DAY = 24 * 12
+
+# Number of possible values
+LABEL_VALUE_NUM = 4
--- traffic_prediction/dataprovider.py
+++ traffic_prediction/dataprovider.py
@@ -1,69 +1,49 @@
-# Copyright (c) 2016 PaddlePaddle Authors, Inc. All Rights Reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from paddle.trainer.PyDataProvider2 import *
+# from paddle.trainer.PyDataProvider2 import *
 import sys
-import numpy as np
-TERM_NUM = 24
-FORECASTING_NUM = 24
-LABEL_VALUE_NUM = 4
+# import numpy as np
 
+from consts import *
 
 def initHook(settings, file_list, **kwargs):
-    """
-    Init hook is invoked before process data. It will set obj.slots and store data meta.
-
-    :param settings: global object. It will passed to process routine.
-    :type obj: object
-    :param file_list: the meta file object, which passed from trainer_config.py,but unused in this function.
-    :param kwargs: unused other arguments.
-    """
+    # Modify a little bit default initHook
+    
     del kwargs  #unused 
 
     settings.pool_size = sys.maxint
-    #Use a time seires of the past as feature.
+    #Use a time seires of the same time in past as feature.
     #Dense_vector's expression form is [float,float,...,float]
     settings.input_types = [dense_vector(TERM_NUM)]
-    #There are next FORECASTING_NUM fragments you need predict.
-    #Every predicted condition at time point has four states.
-    for i in range(FORECASTING_NUM):
-        settings.input_types.append(integer_value(LABEL_VALUE_NUM))
+    settings.input_types.append(integer_value(LABEL_VALUE_NUM))
 
 
+def sequences(size, array):
+    filtered = filter(lambda x: x > -1, array)
+
+    res = list()
+    for i in xrange(len(filtered) - size + 1):
+        res.append(filtered[i:i+size])
+
+    return res
+
+def get_samples(line):
+    _speeds = map(int, line.rstrip('\r\n').split(",")[1:])
+    speeds  = [j - 1 for j in map(float, _speeds)]
+    
+    # Scanning and generating samples
+    for i in xrange(TERM_NUM):
+        for s in sequences(TERMNUM+1, speeds[i::DATES_IN_DAY]):
+            yield s
+    
 @provider(
     init_hook=initHook, cache=CacheType.CACHE_PASS_IN_MEM, should_shuffle=True)
 def process(settings, file_name):
     with open(file_name) as f:
         #abandon fields name
-        f.next()
-        for row_num, line in enumerate(f):
-            speeds = map(int, line.rstrip('\r\n').split(",")[1:])
-            # Get the max index.
-            end_time = len(speeds)
-            # Scanning and generating samples
-            for i in range(TERM_NUM, end_time - FORECASTING_NUM):
-                # For dense slot
-                pre_spd = map(float, speeds[i - TERM_NUM:i])
-
-                # Integer value need predicting, values start from 0, so every one minus 1.
-                fol_spd = [j - 1 for j in speeds[i:i + FORECASTING_NUM]]
-
-                # Predicting label is missing, abandon the sample.
-                if -1 in fol_spd:
-                    continue
-                yield [pre_spd] + fol_spd
-
+        f.readline()
+        for line in f:
+            for sample in get_samples(line):
+                yield sample
+            
 
 def predict_initHook(settings, file_list, **kwargs):
     settings.pool_size = sys.maxint
@@ -74,9 +54,13 @@ def predict_initHook(settings, file_list, **kwargs):
 def process_predict(settings, file_name):
     with open(file_name) as f:
         #abandon fields name
-        f.next()
-        for row_num, line in enumerate(f):
-            speeds = map(int, line.rstrip('\r\n').split(","))
-            end_time = len(speeds)
-            pre_spd = map(float, speeds[end_time - TERM_NUM:end_time])
-            yield pre_spd
+        f.readline()
+        for line in f:
+            item = []
+            for item in get_samples(line):
+                pass
+
+            # skip first element since 
+            yield line[1:] 
+            
+            
