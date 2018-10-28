18206e51d0b47bd87906064cab937b19e89e33c5
Szymon Pajzert <szymonpajzert@gmail.com>
2018-10-28 11:56:49 +0100

    cleanup

--- src/main/scala/jvm/JasminRunner.scala
+++ src/main/scala/jvm/JasminRunner.scala
@@ -2,22 +2,28 @@ package jvm
 
 import java.io._
 
+import jvm.JasminRunner.{StdErr, StdOut}
 
+case class CommandResult(success: Boolean, stdout: StdOut, stderr: StdErr)
+
+object CommandResult {
+  def empty: CommandResult = CommandResult(false, "", "")
+}
 
 object JasminRunner {
-    type StdOut = String
-    type StdErr = String
-    
-    case class CommandResult(success: Boolean, stdout: StdOut, stderr: StdErr)
-    
-    // Simple interface to the run of jasmin.jar on a file
-    // Saves to output file
-    def compile(assemblyCode: String, outputFile: File): CommandResult = {
-        (false, "", "")
-    }
-    
-    def runJava(javaFile: File): CommandResult = {
-        val rt = Runtime.getRuntime();
-        val pr = rt.exec("java -jar map.jar time.rel test.txt debug");
-    }
+  type StdOut = String
+  type StdErr = String
+
+
+  // Simple interface to the run of jasmin.jar on a file
+  // Saves to output file
+  def compile(assemblyCode: String, outputFile: File): CommandResult = {
+    CommandResult.empty
+  }
+
+  def runJava(javaFile: File): CommandResult = {
+    val rt = Runtime.getRuntime();
+    val pr = rt.exec("java -jar map.jar time.rel test.txt debug");
+    CommandResult.empty
+  }
 }
--- src/test/scala/jvm/JasminRunnerSpec.scala
+++ src/test/scala/jvm/JasminRunnerSpec.scala
@@ -4,35 +4,66 @@ import java.io._
 
 import org.scalatest._
 
-class JasminRunnerSpec extends FlatSpec with Matchers {
-    behavior of "JasminRunner"
-    
-    val validCode = """
-    |.class  public Hello
-    |.super  java/lang/Object
-    |
-    |; standard initializer
-    |.method public <init>()V
-    |  aload_0
-    |  invokespecial java/lang/Object/<init>()V
-    |  return
-    |.end method
-    |
-    |.method public static main([Ljava/lang/String;)V
-    |.limit stack 2
-    |  getstatic  java/lang/System/out Ljava/io/PrintStream;
-    |  ldc "Hello"
-    |  invokevirtual  java/io/PrintStream/println(Ljava/lang/String;)V
-    |  return
-    |.end method
-    """.stripMargin
-    
-    it should "compileWithoutErrors" in {
-        val mockFile = File.createTempFile("source", "j")
-        
-        val result = JasminRunner.compile(validCode, mockFile)
-        
-        val array = new Array[Char]
-        assert(!(new FileReader(mockFile).read(array)))
+trait FileMatchers {
+
+  import org.scalatest.matchers._
+
+  class FileBePropertyMatcher extends BePropertyMatcher[java.io.File] {
+    def apply(left: File): BePropertyMatchResult = {
+      val fileNonempty = left.length() != 0
+      BePropertyMatchResult(fileNonempty, "nonempty file")
     }
+  }
+
+  def nonemptyFile = new FileBePropertyMatcher
+}
+
+class FileMatchersTest extends FlatSpec with Matchers with FileMatchers {
+  import java.io.BufferedWriter
+  import java.io.FileWriter
+
+  behavior of "FileMathchers"
+
+  it should "detect empty files" in {
+    val temporaryFile = File.createTempFile("source", "txt")
+
+    val writer: BufferedWriter = new BufferedWriter(new FileWriter(temporaryFile))
+    writer.write("hoho")
+    writer.close()
+
+    temporaryFile should be a nonemptyFile
+  }
+}
+
+class JasminRunnerSpec extends FlatSpec with Matchers with FileMatchers {
+  behavior of "JasminRunner"
+
+  val validCode =
+    """
+      |.class  public Hello
+      |.super  java/lang/Object
+      |
+      |; standard initializer
+      |.method public <init>()V
+      |  aload_0
+      |  invokespecial java/lang/Object/<init>()V
+      |  return
+      |.end method
+      |
+      |.method public static main([Ljava/lang/String;)V
+      |.limit stack 2
+      |  getstatic  java/lang/System/out Ljava/io/PrintStream;
+      |  ldc "Hello"
+      |  invokevirtual  java/io/PrintStream/println(Ljava/lang/String;)V
+      |  return
+      |.end method
+    """.stripMargin
+
+  it should "compileWithoutErrors" in {
+    val mockFile = File.createTempFile("source", "j")
+
+    val result = JasminRunner.compile(validCode, mockFile)
+
+    mockFile should be a nonemptyFile
+  }
 }
